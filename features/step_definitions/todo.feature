62b3c4352f65810e092f4b4843934623e09663b0
william-vz <william.ku@verizon.com>
2021-12-26 16:29:37 -0500

    removed testing files

--- features/step_definitions/todo.feature
+++
@@ -1,6 +0,0 @@
-Feature: Todo
-
-Scenario: Add a Todo task
-    Given I am on the Todo homepage
-    When I create a new Todo task
-    Then I should see my task in the list of all tasks
--- src/components/FilterButton.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import FilterButton from "./FilterButton";
-import { shallow } from "enzyme";
-
-describe("The <FilterButton/> Component", () => {
-  it("should do something", () => {});
-});

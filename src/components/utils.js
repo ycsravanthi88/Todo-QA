4436dcff3abc02115b4fa1dcd3b1aa04035d3f28
william-vz <william.ku@verizon.com>
2022-01-12 15:07:27 -0500

    prettier

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,7 +1,7 @@
 export const INITIAL_LABEL_STATE = {
-    lightgreen: false,
-    lightsalmon: false,
-    mediumpurple: false,
+  lightgreen: false,
+  lightsalmon: false,
+  mediumpurple: false,
 };
 
 export const LABEL_NAMES = Object.keys(INITIAL_LABEL_STATE);
